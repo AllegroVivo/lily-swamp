a518203e2d9936d29aedbbe075b00cf056465aee
Stephanie Phillips <StephBotCoder@gmail.com>
2023-10-02 03:50:52 -0700

    updates

--- UpdateGit.bat
+++ UpdateGit.bat
@@ -3,14 +3,18 @@
 title Lily-Swamp Updater
 echo Welcome to the Lily-Swamp Updater
 echo =================================
+echo. 
 
 echo Adding updated files to Git...
+echo.
 git add *
 
 echo Committing updates...
+echo.
 git commit -m "updates"
 
 echo Pushing to git...
+echo.
 git push
 
 echo Operation Complete!
